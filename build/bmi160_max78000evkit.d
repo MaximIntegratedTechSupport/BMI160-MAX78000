bd87d63e5f8773507064d48523925c463b10c670
Jake Carter <jake.carter@colorado.edu>
2021-09-28 16:38:30 -0500

    Initial commit

---
+++ build/bmi160_max78000evkit.d
@@ -0,0 +1,31 @@
+c:/Users/Jake.Carter/proj/Active/BMI160-MAX78000/build/bmi160_max78000evkit.o: \
+ src/bmi160_max78000evkit.c src/bmi160_max78000evkit.h \
+ BMI160_driver/bmi160.h BMI160_driver/bmi160_defs.h \
+ c:\maximsdk\tools\gnutools\lib\gcc\arm-none-eabi\9.2.1\include\stdint.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\stdint.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\machine\_default_types.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\features.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\_newlib_version.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\_intsup.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\_stdint.h \
+ c:\maximsdk\tools\gnutools\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\math.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\reent.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\_ansi.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\newlib.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\config.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\machine\ieeefp.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\_types.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\machine\_types.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\lock.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\cdefs.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\_ansi.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\string.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\xlocale.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\strings.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\string.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\stdlib.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\machine\stdlib.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\alloca.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/mxc_delay.h
---
+++ build/startup_max78000.d
@@ -0,0 +1,2 @@
+c:/Users/Jake.Carter/proj/Active/BMI160-MAX78000/build/startup_max78000.o: \
+ c:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
