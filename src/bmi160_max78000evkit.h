c7812ce269cf1694b925aa98e19c4a51b9d6afb8
Thomas Lyp <Thomas.Lyp@maximintegrated.com>
2021-09-29 13:35:09 -0700

    Added I2C read and write capabilities. Need further testing, but looks good so far.

--- src/bmi160_max78000evkit.h
+++ src/bmi160_max78000evkit.h
@@ -1,5 +1,7 @@
+//#include <stdint.h>
 #include "bmi160.h"
 #include "mxc_errors.h"
+
 
 // Global bmi160 struct.  Can be used for other driver calls after init_bmi160() is called.
 struct bmi160_dev bmi160;
