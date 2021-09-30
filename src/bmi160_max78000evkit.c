bdc46bc47e6abbcd029bed833a78b589b9600a9a
lypinator <t_lyp@yahoo.com>
2021-09-29 18:55:51 -0700

    Added I2C read, write and initilization. Require more testing, but the main program returns no errors. Need to change the BMI160 address from 0x68 to 0x69 in BMI160_driver files.

--- src/bmi160_max78000evkit.c
+++ src/bmi160_max78000evkit.c
@@ -1,5 +1,30 @@
+#include <stdio.h>
 #include "bmi160_max78000evkit.h"
 #include "mxc_delay.h"
+#include "i2c.h"
+#include "i2c_regs.h"
+
+////////////////////////////
+// Local defines
+////////////////////////////
+#define I2C_MASTER      MXC_I2C2        
+#define I2C_FREQ        500000          //Frequency set in Hz, Max = 1MHz
+#define I2C_CONFIG      1               //Set to 1 for Master, 0 for Slave
+
+////////////////////////////
+// Globals
+////////////////////////////
+mxc_i2c_req_t reqMaster = {
+    .addr = 0x00,
+    .callback = NULL,
+    .i2c = I2C_MASTER,
+    .restart = 0,
+    .rx_buf = NULL,
+    .rx_len = 0,
+    .tx_buf = NULL,
+    .tx_len = 0
+};
+uint8_t error;
 
 ////////////////////////////
 // Local functions
@@ -40,16 +65,74 @@ static void delay_ms(uint32_t ms) {
 }
 
 static int I2C_init() {
-    // TODO: Set up I2C
+    //Initilize the I2C Port as a master
+    if(MXC_I2C_Init(I2C_MASTER, I2C_CONFIG, 0) != E_NO_ERROR){
+        printf("Error initializing the I2C Port 2\n");
+        return E_BAD_PARAM;
+    }
+    
+    //Set the frequency of communication over I2C port
+    if(MXC_I2C_SetFrequency(I2C_MASTER, I2C_FREQ) != E_NO_ERROR){
+        printf("Error setting I2C speed\n");
+        return E_BAD_PARAM;
+    }
+
+
     return E_SUCCESS;
 }
 
 static int8_t i2c_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len) {
-    // TODO
+    
+    //Set paramaters for I2C read
+    reqMaster.addr = dev_addr;          //Set device address 
+    reqMaster.tx_buf = &reg_addr;       //Pointer to register address
+    reqMaster.tx_len = 1;               //Number of bytes to send
+    reqMaster.rx_buf = data;            //Pointer to structure to store read values
+    reqMaster.rx_len = len;             //Number of bytes to read
+
+    if ((error = MXC_I2C_MasterTransaction(&reqMaster)) != E_NO_ERROR) {
+        
+        //Communication error
+        if(error != 1){
+            printf("Error (%d) reading data: Device = 0x%X; Register = 0x%X\n", error, dev_addr, reg_addr);
+            return E_COMM_ERR;
+        }
+        //Message not acknowledged
+        else{
+            printf("Read was not acknowledged: Device = 0x%X; Register = 0x%X\n", dev_addr, reg_addr);
+            return E_COMM_ERR;
+        }
+    }
+
     return BMI160_OK;
 }
 
 static int8_t i2c_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len) {
-    // TODO
+    
+    //Allocate memory for register address and data
+    uint8_t *TXData = (uint8_t *)malloc(sizeof(data)+sizeof(reg_addr));
+    memcpy(TXData, &reg_addr, 1);
+    memcpy(TXData+1, data, len);
+
+    //Set paramaters for I2C read
+    reqMaster.addr = dev_addr;          //Set device address 
+    reqMaster.tx_buf = TXData;          //Pointer to register address
+    reqMaster.tx_len = len+1;           //Number of bytes to send
+    reqMaster.rx_buf = NULL;            //Pointer to structure to store read values
+    reqMaster.rx_len = 0;               //Number of bytes to read
+
+    //Send I2C data
+    if ((error = MXC_I2C_MasterTransaction(&reqMaster)) != E_NO_ERROR) {
+        //Communication error
+        if(error != 1){
+            printf("Error (%d) writing data: Device = 0x%X; Register = 0x%X\n", error, dev_addr, reg_addr);
+            return E_COMM_ERR;
+        }
+        //Message not acknowledged
+        else{
+            printf("Write was not acknowledged: Device = 0x%X; Register = 0x%X\n", dev_addr, reg_addr);
+            return E_COMM_ERR;
+        }
+    }
     return BMI160_OK;
 }
--- src/main.c
+++ src/main.c
@@ -3,21 +3,28 @@
 
 // This test program shall initialize the BMI160, validate the built-in self-test, and test additional bmi160 driver functions.
 int main() {
-    int8_t result;
 
+    volatile int8_t result;
     if ( (result = init_bmi160()) != E_SUCCESS) {
-        // TODO: Handle failure
+        // TODO:  Handle failure
+    
     }
-
+    printf("Result for Initialization: %d\n",result);
+    
     if ( (result = bmi160_perform_self_test(BMI160_ACCEL_ONLY, &bmi160)) != BMI160_OK) {
         // TODO:  Handle failure
     }
+
+    printf("Result for Self Test Accelerometer: %d\n",result);
 
     if ( (result = bmi160_perform_self_test(BMI160_GYRO_ONLY, &bmi160)) != BMI160_OK) {
         // TODO:  Handle failure
     }
 
-    // TODO: test additional driver functions
+    printf("Result for Self Test Gyrescope: %d\n",result);
 
+    // TODO: test additional driver functions
+    while(1){
+    }
     return 1;
 }
